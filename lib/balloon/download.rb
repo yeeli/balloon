fe6bf1d7b4e9524842dfc6089194d8a34828f933
ye.li <yeeli@outlook.com>
2020-06-26 15:35:35 +0800

    fixed error name error

--- lib/balloon/download.rb
+++ lib/balloon/download.rb
@@ -6,23 +6,23 @@ module Balloon
     extend ActiveSupport::Concern
 
     def down_url(uri)
-      connection = ::Faraday.new 
+      connection = ::Faraday.new
       response = connection.get(uri)
       generate_down_cache_directory
       path = File.join down_cache_path, generate_down_id
-      
+
       if response.status != 200
         raise Balloon::DownloadError, I18n.translate(:"errors.messages.response_error")
       end
-      
+
       if content_type = MIME::Types[response.headers["content-type"]][0]
         mime_type = content_type.content_type
         if self.respond_to?(:uploader_mimetype_white)
-           if !uploader_mimetype_white.include?(mime_type)          
+           if !uploader_mimetype_white.include?(mime_type)
              raise Balloon::DownloadError, I18n.translate(:"errors.messages.down_mime_error")
            end
         elsif self.respond_to?(:uploader_mimetype_black)
-           if !uploader_mimetype_black.include?(mime_type)          
+           if !uploader_mimetype_black.include?(mime_type)
              raise Balloon::DownloadError, I18n.translate(:"errors.messages.down_mime_error")
            end
         end
@@ -31,11 +31,11 @@ module Balloon
       File.open(path, "wb") do |f|
         f.write(response.body)
       end
-      
+
       return path
-    rescue Faraday::Error::ConnectionFailed => e
+    rescue Faraday::ConnectionFailed => e
       raise Balloon::DownloadError, I18n.translate(:"errors.messages.connection_failed")
-    rescue Faraday::Error::TimeoutError => e
+    rescue Faraday::TimeoutError => e
       raise Ballloon::DownloadError, I18n.translate(:"errors.messages.timeout_error")
     end
 
